c45fed1fde26551b8ebd7bc4ff72428a88a17ef6
Cody Nguyen <cody.haind@gmail.com>
2018-11-14 08:39:00 +0700

    integrate footer

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -2,7 +2,7 @@
   <div class="footer">
     <nav class="inner">
       <div class="footer__left">
-        <img src="/images/stockchase-by-wealthica-white-logo.svg" width="190" style="float: left;">
+        <img src="~assets/svgs/stockchase-by-wealthica-white-logo.svg" width="190" style="float: left;">
       </div>
       <div class="footer__center">
         <div style="width: 87%;">
@@ -12,23 +12,23 @@
         </div>
 
         <ul class="footer-list-menu">
-          <li><a href="https://stockchase.com/page/view/about-us">About us</a></li>
-          <li><a href="https://stockchase.com/blog/post/how-this-site-works">How to use</a></li>
-          <li><a href="https://stockchase.com/page/view/in-the-news">In the news</a></li>
-          <li><a href="https://stockchase.com/page/view/advertising">Advertising</a></li>
+          <li><a :href="appUrl + '/page/view/about-us'">About us</a></li>
+          <li><a :href="appUrl + '/blog/post/how-this-site-works'">How to use</a></li>
+          <li><a :href="appUrl + '/page/view/in-the-news'">In the news</a></li>
+          <li><a :href="appUrl + '/page/view/advertising'">Advertising</a></li>
           <li><a href="mailto:hello@stockchase.com">@Contact us</a></li>
         </ul>
 
         <ul class="footer-list-menu" style="margin-top:0;">
-          <li><a href="https://stockchase.com/page/view/privacy">Privacy Policy</a></li>
-          <li><a href="https://stockchase.com/page/view/privacy">Terms of service</a></li>
+          <li><a :href="appUrl + '/page/view/privacy'">Privacy Policy</a></li>
+          <li><a :href="appUrl + '/page/view/privacy'">Terms of service</a></li>
         </ul>
 
         <div style="float: right; margin-top: -47px;">
-          <a href="https://stockchase.com/rss/toppicks" style="margin-right: 7px;"><img src="/images/rss-light-icon.png" width="35"></a>
-          <a href="https://stockchase.com/rss/daily" style="margin-right: 7px;"><img src="/images/rss-light-icon.png" width="35"></a>
-          <a href="https://www.facebook.com/stockchase" style="margin-right: 7px;"><img src="/images/facebook-footer-icon.svg" width="35"></a>
-          <a href="http://twitter.com/stockchase" style="margin-right: 0;"><img src="/images/twitter-footer-icon.svg" width="35"></a>
+          <a :href="appUrl + '/rss/toppicks'" style="margin-right: 7px;"><img src="~assets/images/rss-light-icon.png" width="35"></a>
+          <a :href="appUrl + '/rss/daily'" style="margin-right: 7px;"><img src="~assets/images/rss-light-icon.png" width="35"></a>
+          <a href="https://www.facebook.com/stockchase" style="margin-right: 7px;"><img src="~assets/svgs/facebook-footer-icon.svg" width="35"></a>
+          <a href="http://twitter.com/stockchase" style="margin-right: 0;"><img src="~assets/svgs/twitter-footer-icon.svg" width="35"></a>
         </div>
       </div>
       <div style="clear: both;"></div>
@@ -56,6 +56,44 @@ export default {
   border-top 1px solid #E9E9EA
   padding 0 20px
   background #605E5E
+
+  &__left
+    width 15%
+    float left
+    padding-left 24px
+    height 220px
+
+  &__center
+    float left
+    width 82%
+    padding-left 70px
+    box-sizing border-box
+    padding-right 20px
+    height 220px
+
+    p
+      margin-top 0
+      line-height 1.38em
+      margin-bottom 1.4em
+      font-size 15px
+      text-align left
+      color #FFFFFF
+      line-height 24px
+
+  &-list-menu
+    padding 0
+    margin 0
+    margin-top -5px
+
+    li
+      display inline-block
+      margin-right 10px
+      font-size 14pt
+      line-height 1.6
+      font-weight 500
+      a
+        color #FFFFFF
+        font-size 15px
 
   .inner
     box-sizing border-box
