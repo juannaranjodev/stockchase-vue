f961ce45ba788240c9feec80b14b08cb77de0152
Simon Boulet <simon@nostalgeek.com>
2018-12-07 11:01:19 -0500

    Implement pagination getAdjacent Dates

--- models/opinion.js
+++ models/opinion.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const moment = require('moment');
+const _ = require('lodash');
 
 module.exports = (sequelize, DataTypes) => {
   var Opinion = sequelize.define('Opinion', {
@@ -64,19 +65,52 @@ module.exports = (sequelize, DataTypes) => {
   // Get the date of the most recent opinion
   Opinion.getRecentOpinionDate = async function() {
     var recentOpinion = await Opinion.findOne({
-      where: { company_id: { $ne: 1970 } }, // ignore market comment
+      where: { company_id: { $ne: 1970 } }, // Ignore market comments
       order: [['date', 'DESC'], ['id', 'ASC']]
     });
+
     return recentOpinion.date;
   };
 
-  // Get adjacent dates that have opinions (for pagination)
-  Opinion.getAdjacentOpinionDates = async function(date) {
-    const startDiff = Math.min(3, moment().diff(moment(date, 'YYYY-MM-DD'), 'days'));
+  // Get the date of the most recent market comment
+  Opinion.getRecentMarketCommentDate = async function() {
+    var recentOpinion = await Opinion.findOne({
+      where: { company_id: 1970 }, // Market comments
+      order: [['date', 'DESC'], ['id', 'ASC']]
+    });
 
-    // count down to older dates
-    return [...Array(7)].map((_, i) => {
-      return moment(date, 'YYYY-MM-DD').add(startDiff - i, 'day').format('YYYY-MM-DD');
+    return recentOpinion.date;
+  };
+
+  // Given a date returns the number of opinions for adjacent dates (3 days prior, and 3 days ahead)
+  // Used for pagination
+  Opinion.getAdjacentOpinionDates = async function(date) {
+    return sequelize.query([
+      '( SELECT Date,COUNT(*) FROM New_opinion WHERE Date <= :date AND company_id != 1970 GROUP BY Date ORDER BY Date DESC LIMIT 3 )',
+      'UNION',
+      '( SELECT Date,COUNT(*) FROM New_opinion WHERE Date >= :date AND company_id != 1970 GROUP BY Date ORDER BY Date ASC LIMIT 3 )',
+      'ORDER BY Date',
+    ].join(' '), {
+      replacements: { date: moment(date).format('YYYY-MM-DD') },
+      type: sequelize.QueryTypes.SELECT,
+    }).then(function(datesCount) {
+      return _.map(datesCount, 'Date');
+    });
+  };
+
+  // Given a date returns the number of market comments for adjacent dates (3 days prior, and 3 days ahead)
+  // Used for pagination
+  Opinion.getAdjacentMarketCommentDates = async function(date) {
+    return sequelize.query([
+      '( SELECT Date,COUNT(*) FROM New_opinion WHERE Date <= :date AND company_id = 1970 GROUP BY Date ORDER BY Date DESC LIMIT 3 )',
+      'UNION',
+      '( SELECT Date,COUNT(*) FROM New_opinion WHERE Date >= :date AND company_id = 1970 GROUP BY Date ORDER BY Date ASC LIMIT 3 )',
+      'ORDER BY Date',
+    ].join(' '), {
+      replacements: { date: moment(date).format('YYYY-MM-DD') },
+      type: sequelize.QueryTypes.SELECT,
+    }).then(function(datesCount) {
+      return _.map(datesCount, 'Date');
     });
   };
 
@@ -89,7 +123,16 @@ module.exports = (sequelize, DataTypes) => {
   // Get opinions for a given date
   Opinion.getOpinionsByDate = function(date) {
     return Opinion.findAll({
-      where: { date: date, company_id: { $ne: 1970 } }, // ignore market comment
+      where: { date: date, company_id: { $ne: 1970 } }, // Ignore market comments
+      order: [['date', 'DESC'], ['id', 'ASC']],
+      include: [ { all: true, nested: true } ],
+    });
+  };
+
+  // Get market comments for a given date
+  Opinion.getMarketCommentsByDate = function(date) {
+    return Opinion.findAll({
+      where: { date: date, company_id: 1970 }, // Market comments
       order: [['date', 'DESC'], ['id', 'ASC']],
       include: [ { all: true, nested: true } ],
     });
@@ -124,14 +167,6 @@ module.exports = (sequelize, DataTypes) => {
     return recentOpinion.date;
   };
 
-  // Get market comments for a given date
-  Opinion.getMarketCommentsByDate = function(date) {
-    return Opinion.findAll({
-      where: { date: date, company_id: 1970 }, // ignore market comment
-      order: [['date', 'DESC'], ['id', 'ASC']],
-      include: [ { all: true, nested: true } ],
-    });
-  };
 
   // Given a date return the most recent following date that has a market comment (used for pagination)
   Opinion.getNewerMarketCommentDate = async function(date) {
