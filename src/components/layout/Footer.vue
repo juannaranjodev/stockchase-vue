f40df9407e0c716a1377b608480fd5dc3ca97f0b
Cody Nguyen <cody.haind@gmail.com>
2018-12-11 12:29:47 +0700

    Remove Wealthica from Copyright in the footer

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -2,17 +2,17 @@
   <div class="footer">
     <nav class="inner">
       <div class="footer__left">
-        <img 
-          src="~assets/svgs/stockchase-by-wealthica-white-logo.svg" 
-          width="190" 
+        <img
+          src="~assets/svgs/stockchase-by-wealthica-white-logo.svg"
+          width="190"
           style="float: left;">
       </div>
       <div class="footer__center">
         <div style="width: 87%;">
           <p>Stockchase,  in its reporting on what has been discussed by individuals on business television programs (in particular Business News Network), neither recommends nor promotes any investment strategies.</p>
 
-          <p>We paraphrase the experts by hand, we watch the shows and write down what we understood from the experts’ comments. <span style="font-weight: bold;">We are human and can make mistakes</span>, help us fix any errors. If you see something that you know is not right or if there is a problem with the site, feel free to email us at : <a 
-            href="mailto:hello@stockchase.com" 
+          <p>We paraphrase the experts by hand, we watch the shows and write down what we understood from the experts’ comments. <span style="font-weight: bold;">We are human and can make mistakes</span>, help us fix any errors. If you see something that you know is not right or if there is a problem with the site, feel free to email us at : <a
+            href="mailto:hello@stockchase.com"
             style="color: #FFFFFF;">hello@stockchase.com</a>.</p>
         </div>
 
@@ -30,31 +30,31 @@
         </ul>
 
         <div class="footer-social-links">
-          <a 
-            :href="appUrl + '/rss/toppicks'" 
-            style="margin-right: 7px;"><img 
-              src="~assets/images/rss-light-icon.png" 
+          <a
+            :href="appUrl + '/rss/toppicks'"
+            style="margin-right: 7px;"><img
+              src="~assets/images/rss-light-icon.png"
               width="35"></a>
-          <a 
-            :href="appUrl + '/rss/daily'" 
-            style="margin-right: 7px;"><img 
-              src="~assets/images/rss-light-icon.png" 
+          <a
+            :href="appUrl + '/rss/daily'"
+            style="margin-right: 7px;"><img
+              src="~assets/images/rss-light-icon.png"
               width="35"></a>
-          <a 
-            href="https://www.facebook.com/stockchase" 
-            style="margin-right: 7px;"><img 
-              src="~assets/svgs/facebook-footer-icon.svg" 
+          <a
+            href="https://www.facebook.com/stockchase"
+            style="margin-right: 7px;"><img
+              src="~assets/svgs/facebook-footer-icon.svg"
               width="35"></a>
-          <a 
-            href="http://twitter.com/stockchase" 
-            style="margin-right: 0;"><img 
-              src="~assets/svgs/twitter-footer-icon.svg" 
+          <a
+            href="http://twitter.com/stockchase"
+            style="margin-right: 0;"><img
+              src="~assets/svgs/twitter-footer-icon.svg"
               width="35"></a>
         </div>
       </div>
       <div style="clear: both;"/>
     </nav>
-    <div class="copyright">© Stockchase Inc. & Wealthica Financial Technology Inc.</div>
+    <div class="copyright">© Stockchase Inc.</div>
   </div>
 </template>
 
