0db561de0cc09bd7f62aa02d1aeff17fd758c42b
Cody Nguyen <cody.haind@gmail.com>
2019-03-07 19:15:40 +0700

    add Premium block

--- src/components/home/MarketCallGuest.vue
+++ src/components/home/MarketCallGuest.vue
@@ -92,10 +92,86 @@
 
     <div
       v-if="guest.hasRating"
-      class="guest__section guest__section--rating"
+      class="guest__section guest__section--premium"
     >
       <div class="guest__section-left">
-        left
+        <div class="premium">
+          <div class="premium__badge">
+            <div class="premium__label">
+              Premium
+            </div>
+          </div>
+          <div class="premium__content">
+            <div class="premium__title">
+              <a
+                class="premium__link premium__link--big"
+                href="#"
+              >Chris Stuchberry’s Top Picks Portfolio</a>
+              <div class="premium__rating">
+                <img
+                  src="~assets/images/starts-rate.png"
+                  height="27">
+                <a
+                  class="premium__link"
+                  href="#"
+                >Reveal Performance</a>
+              </div>
+            </div>
+            <div class="premium__info">
+              <div class="premium__figure">
+                <a
+                  class="premium__company-logo"
+                  href="#">
+                  <img src="~assets/images/stock-nologo.png">
+                </a>
+                <div class="premium__company-badge">
+                  <img
+                    src="~assets/svgs/top-pick-star.svg"
+                    width="20"
+                    height="20"
+                  >
+                </div>
+              </div>
+
+              <div class="premium__info-item">
+                <div class="premium__info-item-label">TOP PICKS</div>
+                <div class="premium__info-item-content">
+                  <div class="premium__info-item-value premium__info-item-value--masked">
+                    masked
+                  </div>
+                </div>
+              </div>
+
+              <div class="premium__info-item">
+                <div class="premium__info-item-label">BIG WIN</div>
+                <div class="premium__info-item-content">
+                  <div class="premium__info-item-value">
+                    8
+                  </div>
+                </div>
+              </div>
+
+              <div class="premium__signup">
+                <div class="premium__signup-title">This is a Premium Subscriber-Only Feature</div>
+                <a
+                  href="#"
+                  class="premium__signup-button"
+                >
+                  <span>Sign up for Premium</span>
+                </a>
+              </div>
+
+              <div class="premium__info-item">
+                <div class="premium__info-item-label">HORIZON</div>
+                <div class="premium__info-item-content">
+                  <div class="premium__info-item-value premium__info-item-value--masked">
+                    1 Month
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div class="guest__section-right" />
     </div>
@@ -134,7 +210,7 @@ export default {
     margin-bottom 25px
     min-width 0
 
-    &--rating
+    &--premium
       border-radius 0
       border 0
 
@@ -145,7 +221,7 @@ export default {
       flex 1
       min-width 0
 
-      .guest-section--rating &
+      .guest__section--premium &
         border 1px solid #E8E8E8
 
     &-right
@@ -158,6 +234,7 @@ export default {
       align-items stretch
 
   &__avatar
+    width 68px
     flex-grow 0
     flex-shrink 0
     border-top-left-radius 4px
@@ -171,7 +248,7 @@ export default {
       display flex
       width 45px
       height 45px
-      margin 25px 10px
+      margin 25px 0
 
     img
       width 45px
@@ -300,5 +377,136 @@ export default {
   &__more
     color red
     text-decoration underline
+
+.premium
+  height 137px
+  display flex
+  align-items stretch
+  flex 1
+  min-width 0
+
+  &__badge
+    width 68px
+    flex-grow 0
+    flex-shrink 0
+    background-color #EEEBEB
+    display flex
+    align-items center
+    justify-content center
+
+  &__label
+    transform rotate(-90deg)
+    color #FF3B43
+    font-size 18px
+    font-weight bold
+    text-transform uppercase
+
+  &__content
+    flex 1
+
+  &__title
+    display flex
+    align-items center
+    justify-content space-between
+    height 55px
+    padding 0 15px
+
+  &__rating
+    display flex
+    align-items center
+
+    img
+      margin-right 5px
+
+  &__link
+    font-size 14px
+    color #FF3B43
+
+    &--big
+      font-size 18px
+      font-weight bold
+
+  &__figure
+    width 75px
+    display flex
+    align-items center
+    position relative
+    flex-grow 0
+    flex-shrink 0
+
+  &__company
+    &-logo
+      width 47px
+      height 47px
+      border-radius 5px
+      border 1px solid #ccc
+      background white
+      position relative
+      display block
+
+      img
+        position absolute
+        top 0
+        left 0
+        right 0
+        bottom 0
+        margin auto
+        max-width 85%
+        max-height 85%
+        width auto
+        height auto
+
+    &-badge
+      position absolute
+      left 35px
+      top -10px
+
+  &__info
+    padding 10px 15px 0
+    display flex
+    align-items center
+
+    &-item
+      &:not(:last-child)
+        margin-right 15px
+
+      &-value
+        &--masked
+          color transparent
+          text-shadow 0 0 10px rgba(0,0,0,0.5)
+
+  &__signup
+    width 302px
+    height 49px
+    border 1px solid red
+    text-align center
+    border-radius 5px
+    margin-right 15px
+
+    &-title
+      margin-top 10px
+      font-size 13px
+      color #717171
+      font-weight bold
+
+    &-button
+      width 60%
+      cursor pointer
+      padding 0 15px
+      text-align center
+      border-radius 4px
+      border 1px solid #FF5030
+      background #FF5030
+      color white !important
+      height 29px
+      line-height 29px
+      display flex
+      align-items center
+      justify-content center
+      margin 5px auto 0
+
+      &:hover
+        background #FF2E50
+        color white
 
 </style>
