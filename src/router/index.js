0f98cbf7b36339285c024d106e45d8d72bc755c0
Jovanni G <bugsworld85@gmail.com>
2019-04-01 11:00:26 -0400

    assigned Experts.vue import to a variable

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ Vue.use(Router)
 
 // route-level code splitting
 const createListView = type => () => import('../views/CreateListView').then(m => m.default(type))
+const expertsView = import('../views/Experts.vue')
 
 export function createRouter () {
   return new Router({
@@ -18,8 +19,8 @@ export function createRouter () {
       { path: '/opinions/market/:date/:page', component: createListView('comments') },
       { path: '/opinions/:date', component: createListView('opinions') },
       { path: '/opinions/:date/:page', component: createListView('opinions') },
-      { path: '/expert', component: () => import('../views/Experts.vue') },
-      { path: '/expert/index/all/:type/sort/:sort/page/:page/direction/:direction/max/:itemsPerPage', component: () => import('../views/Experts.vue') },
+      { path: '/expert', component: () => expertsView },
+      { path: '/expert/index/all/:type/sort/:sort/page/:page/direction/:direction/max/:itemsPerPage', component: () => expertsView },
     ]
   })
 }
