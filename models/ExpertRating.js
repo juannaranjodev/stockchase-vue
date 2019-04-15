52e14e75a63c42f41f4c95fe886eb78e77033b76
lotgitp <riktechle@hotmail.com>
2019-04-15 14:12:57 -0400

    Updated expert rating card url

--- models/ExpertRating.js
+++ models/ExpertRating.js
@@ -49,6 +49,17 @@ module.exports = (sequelize, DataTypes) => {
     });
   };
 
+  function titleCase(str) {
+    const urlStr = str.toLowerCase()
+      .replace(/[^\w ]+/g, '')
+      .replace(/ +/g, '-');
+    const splitStr = urlStr.toLowerCase().split('-');
+    for (let i = 0; i < splitStr.length; i++) {
+      splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    }
+    return splitStr.join('-');
+  }
+
   // Get top/worst experts
   ExpertRating.getTopOrWorstExperts = async function (top = true, limit = 25) {
     const order = top ? 'DESC' : 'ASC';
@@ -101,7 +112,7 @@ module.exports = (sequelize, DataTypes) => {
         if (beforeExpertId !== expert.expert_id) {
           result.expertRank = expertRank++;
           result.avatar = expert.avatar ? `https://stockchase.s3.amazonaws.com/${expert.avatar}` : '/assets/svg/expert_profile_default.svg';
-          result.url = `/expert/view/${expert.expert_id}/${this.name.replace(/\W+/g, ' ').replace(/\s+/g, '-')}`;
+          result.url = `/expert/view/${expert.expert_id}/${titleCase(expert.name)}/rating`;
           totalWinsById[beforeExpertId] = totalWins;
           totalLosesById[beforeExpertId] = totalLoses;
           beforeExpertId = expert.expert_id;
