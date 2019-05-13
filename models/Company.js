juannaranjodev/stockchase-vue

commit 35f9b61cf4f1ae79ab4a846d9b92274ce0c1b68c

fixed: used regex instead of multiple replace

models/Company.js
@@ -23,13 +23,13 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.VIRTUAL,
       get() {
         const baseUrl = process.env.APP_URL || 'https://stockchase.com';
-        return `https://data.wealthica.com/api/securities/${this.symbol.toLowerCase().replace(' (dead)', '').replace(/[()]/g, '')}/logo?default=${baseUrl}/assets/no_logo.png`;
+        return `https://data.wealthica.com/api/securities/${this.symbol.replace(/ ?\([^\)]+\)/g, '')}/logo?default=${baseUrl}/assets/no_logo.png`;
       },
     },
     url: {
       type: DataTypes.VIRTUAL,
       get() {
-        return `/company/view/${this.id}/${this.symbol.toLowerCase().replace(' (dead)', '').replace(/[()]/g, '')}`;
+        return `/company/view/${this.symbol.replace(/ ?\([^\)]+\)/g, '')}`;
       },
     },
     active_original: {

commit cf0687b816aa17b977e8f150e766f388888579d2

Fix broken company page URL (introduced in 35f9b61) (#81)

models/Company.js
@@ -29,7 +29,7 @@ module.exports = (sequelize, DataTypes) => {
     url: {
       type: DataTypes.VIRTUAL,
       get() {
-        return `/company/view/${this.symbol.replace(/ ?\([^)]+\)/g, '')}`;
+        return `/company/view/${this.id}/${this.symbol.replace(/ ?\([^)]+\)/g, '')}`;
       },
     },
     active_original: {