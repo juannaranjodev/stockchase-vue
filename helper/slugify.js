20be2a31269301536ebac644babd9b90b30d1a35
lotgitp <riktechle@hotmail.com>
2019-05-15 09:41:10 -0400

    Updated slugify helper function

--- helper/slugify.js
+++ helper/slugify.js
@@ -1,7 +1,6 @@
-const { titleize } = require('inflection');
-
 function slugify(string) {
-  return titleize(string.toString().toLowerCase().replace(/[^\s\w]+/g, ''))
+  return string.toString()
+    .replace(/[^\s\w]+/g, '')
     .replace(/\s+/g, '-')
     .replace(/^-+|-+$/g, '');
 }
